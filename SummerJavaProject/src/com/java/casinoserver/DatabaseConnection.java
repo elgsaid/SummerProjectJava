f8d9bc5026c34f7a8c7b982b3de4c4e0b192c032
sa3047 <swapnil_aher@yahoo.co.in>
2015-06-23 21:30:54 -0400

    Connection class updated for sql Injection attack

--- SummerJavaProject/src/com/java/casinoserver/DatabaseConnection.java
+++ SummerJavaProject/src/com/java/casinoserver/DatabaseConnection.java
@@ -1,5 +1,6 @@
 package com.java.casinoserver;
 import java.sql.*;
+import java.util.ArrayList;
 /*
  * @Author: Swapnil Aher
  * 
@@ -28,17 +29,46 @@ public class DatabaseConnection {
 		return sqliteConn;
 	}
  
-	public ResultSet runSelectSql(String sql, Connection cn) throws SQLException {
+	public ResultSet runSelectSql(String sql, Connection cn,ArrayList<String> Userdata) throws SQLException {
 		System.out.println("In Select Statement...."+sql);
-		Statement sta = cn.createStatement();
-		return sta.executeQuery(sql);
+		
+		/*Statement sta = cn.createStatement();
+		return sta.executeQuery(sql);*/
+		
+		 PreparedStatement ps = null;
+		 ps = cn.prepareStatement(sql);
+		 ps.setString(1, Userdata.get(1).toUpperCase());
+		 return ps.executeQuery();
 	}
 	
-	public void runUpdateSql(String sql, Connection cn) throws SQLException {
+	public void runUpdateSql(String sql, Connection cn,ArrayList<String> Userdata ) throws SQLException {
 		
 		 System.out.println("In DB update function .. "+sql);
-		 Statement sta = cn.createStatement();
-		 sta.executeUpdate(sql);
+		/* Statement sta = cn.createStatement();
+		 sta.executeUpdate(sql);*/
+
+		 PreparedStatement ps = null;
+		 ps = cn.prepareStatement(sql);
+		 
+		 ps.setInt(1,Integer.parseInt(Userdata.get(3).toString()));
+		 ps.setInt(2, Integer.parseInt(Userdata.get(2).toString()));
+		 
+		 int result= ps.executeUpdate();
+		 System.out.println(result);
+		 
+	}
+
+	public void runInsertSql(String sql, Connection cn,ArrayList<String> Userdata ) throws SQLException {
+		
+		 System.out.println("In DB update function .. "+sql);
+		/* Statement sta = cn.createStatement();
+		 sta.executeUpdate(sql);*/
+
+		 PreparedStatement ps = null;
+		 ps = cn.prepareStatement(sql);
+		 ps.setString(1, Userdata.get(1).toUpperCase());
+		 ps.executeUpdate();
+			
 	}
 
 	/*
@@ -57,9 +87,15 @@ public class DatabaseConnection {
 			DatabaseConnection dc = new DatabaseConnection();
 			cn = dc.getDatabaseConnection("Casino");
 			String name = "'swapnil'";
-		    //dc.runUpdateSql("update User set Money ="+ LoginDetails.UserBalance + " where id =" + LoginDetails.UserId, cn);
+		    
+			ArrayList<String> Userdata = new ArrayList<String>();
+			Userdata.add("Update");
+			Userdata.add("mukhesh");
+			Userdata.add("1");
+			Userdata.add("750");
 			
-			ResultSet rs = dc.runSelectSql("Select * from User", cn);
+			dc.runUpdateSql("update User set Money =? where id =?", cn,Userdata);
+			ResultSet rs = dc.runSelectSql("Select * from User where Upper(Name)== ?", cn,Userdata);
 			while (rs.next()) {
 				// read the result set
 				System.out.println("name = " + rs.getString("Name"));
@@ -67,8 +103,11 @@ public class DatabaseConnection {
 				System.out.println("Money = " + rs.getInt("Money"));
 			}
 		    
+			dc.runInsertSql("INSERT INTO User (Name,Money) values (?,200);", cn, Userdata);
+			
 		} catch (Exception e) {
 			// TODO: handle exception
+			System.out.println(e.toString());
 		} finally {
 
 			if (cn != null)
